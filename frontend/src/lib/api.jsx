0f32cd601e8ec1bb1deaf7a9ed8bfc4fad7670fc
202412079-rajvi <202412079@dau.ac.in>
2025-10-23 20:33:09 +0530

    final commit

---
+++ frontend/src/lib/api.jsx
@@ -0,0 +1,33 @@
+import axios from "axios";
+import Cookies from "js-cookie";
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+
+const api = axios.create({
+  baseURL: API_URL,
+  withCredentials: true,
+});
+
+api.interceptors.request.use(
+  (config) => {
+    const token = Cookies.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
+
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      Cookies.remove("token");
+      window.location.href = "/login";
+    }
+    return Promise.reject(error);
+  }
+);
+
+export { api };
